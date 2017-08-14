daade373658a3d99801b8cde00ac52eb9393f0d6
Ben Noack <benjamin.noack@icloud.com>
2017-08-14 19:39:27 -0400

    check mma7760 init results

--- lib/mma7760/mma7760.c
+++ lib/mma7760/mma7760.c
@@ -6,11 +6,11 @@ mma7760_mode_t mma7760_mode = MMA7760_STANDBY;
 mma7760_mode_t init_mma7760(void) {
   //set mode to active
   char set_active_buf[2] = {MMA7760_MODE, MMA7760_ACTIVE};
-  write_i2c(MMA7760_ADDRESS, set_active_buf, 2);
-  //TODO check result
-  mma7760_mode = MMA7760_ACTIVE;
-  //TODO set the sample rate
-  return MMA7760_ACTIVE;
+  if (write_i2c(MMA7760_ADDRESS, set_active_buf, 2) == I2C_OK) {
+    mma7760_mode = MMA7760_ACTIVE;
+  }
+
+  return mma7760_mode;
 }
 
 int8_t read_mma7760_axis(mma7760_axis_t axis) {
