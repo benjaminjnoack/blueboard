ae6b19d39965152d8f0bf3a2b8591506b8d69e41
Ben Noack <benjamin.noack@icloud.com>
2017-08-13 23:32:53 -0400

    [27] using LED constants in functions.

--- lib/leds/leds.c
+++ lib/leds/leds.c
@@ -1,39 +1,13 @@
 #include "leds.h"
 
 void init_leds(void) {
-  LPC_GPIO1->FIODIR2 |= ((1 << 2) | (1 << 4) | (1 << 5) | (1 << 7));
+  LPC_GPIO1->FIODIR2 |= (LED1 | LED2 | LED3 | LED4);
 }
 
 void led_off(led_t led) {
-  switch (led) {
-    case LED1:
-      LPC_GPIO1->FIOPIN2 &= ~(1 << 2);
-      break;
-    case LED2:
-      LPC_GPIO1->FIOPIN2 &= ~(1 << 4);
-      break;
-    case LED3:
-      LPC_GPIO1->FIOPIN2 &= ~(1 << 5);
-      break;
-    case LED4:
-      LPC_GPIO1->FIOPIN2 &= ~(1 << 7);
-      break;
-  }
+  LPC_GPIO1->FIOPIN2 &= ~(led);
 }
 
 void led_on(led_t led) {
-  switch (led) {
-    case LED1:
-      LPC_GPIO1->FIOPIN2 |= (1 << 2);
-      break;
-    case LED2:
-      LPC_GPIO1->FIOPIN2 |= (1 << 4);
-      break;
-    case LED3:
-      LPC_GPIO1->FIOPIN2 |= (1 << 5);
-      break;
-    case LED4:
-      LPC_GPIO1->FIOPIN2 |= (1 << 7);
-      break;
-  }
+  LPC_GPIO1->FIOPIN2 |= led;
 }
