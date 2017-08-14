936701c8b7fac3817d9e9b756238f5ff4fdc8985
Ben Noack <benjamin.noack@icloud.com>
2017-08-14 19:33:10 -0400

    setting active mode correctly closes #24

--- lib/mma7760/mma7760.c
+++ lib/mma7760/mma7760.c
@@ -5,7 +5,7 @@ mma7760_mode_t mma7760_mode = MMA7760_STANDBY;
 
 mma7760_mode_t init_mma7760(void) {
   //set mode to active
-  char set_active_buf[2] = {MMA7760_MODE, (1 << 1)};
+  char set_active_buf[2] = {MMA7760_MODE, MMA7760_ACTIVE};
   write_i2c(MMA7760_ADDRESS, set_active_buf, 2);
   //TODO check result
   mma7760_mode = MMA7760_ACTIVE;
--- src/main.c
+++ src/main.c
@@ -1,10 +1,12 @@
 #include "blueboard.h"
 
 int main(int argc, char const *argv[]) {
-  init_pwm();
-  init_rgb();
+  init_i2c();
+  init_mma7760();
 
   while (1) {
+    read_mma7760_axis(Y_AXIS);
+    delay();
   }
 
   return 0;
