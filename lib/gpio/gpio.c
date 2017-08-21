4473849bbcbba04dc5dc03bd7bd76ad36c759737
Ben Noack <benjamin.noack@icloud.com>
2017-08-20 22:41:09 -0400

    correct GPIO mapping

--- lib/gpio/gpio.c
+++ lib/gpio/gpio.c
@@ -4,10 +4,10 @@ void init_gpio() {
   LPC_GPIO0->FIODIR |= (PIN_6 | PIN_8 | PIN_11 | PIN_29 | PIN_30);
 }
 
-void gpio_set(gpio_t gpio) {
+void set_gpio(gpio_t gpio) {
   LPC_GPIO0->FIOSET |= (gpio);
 }
 
-void gpio_clear(gpio_t gpio) {
+void clear_gpio(gpio_t gpio) {
   LPC_GPIO0->FIOCLR |= (gpio);
 }
--- lib/gpio/gpio.h
+++ lib/gpio/gpio.h
@@ -1,13 +1,13 @@
 #include "LPC17xx.h"
 
 typedef enum {
-  PIN_6 = (1 << 6),   //P0.6  LCD A0
-  PIN_8 = (1 << 8),   //P0.8  LCD Reset
-  PIN_11 = (1 << 11), //P0.18 LCD CS
+  PIN_6 = (1 << 8),   //P0.8  LCD Reset
+  PIN_8 = (1 << 6),   //P0.6  LCD A0
+  PIN_11 = (1 << 18), //P0.18 LCD CS
   PIN_29 = (1 << 5),  //P0.5  XBee Status
   PIN_30 = (1 << 4)   //P0.4  XBee Reset
 } gpio_t;
 
 void init_gpio();
-void gpio_set(gpio_t gpio);
-void gpio_clear(gpio_t gpio);
+void set_gpio(gpio_t gpio);
+void clear_gpio(gpio_t gpio);
