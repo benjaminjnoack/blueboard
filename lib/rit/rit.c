0dd8aa46425f54e2ccfcbeac80d24a11dec93134
Ben Noack <benjamin.noack@icloud.com>
2017-08-29 01:17:28 -0400

    set rit pclk

--- lib/rit/rit.c
+++ lib/rit/rit.c
@@ -1,12 +1,14 @@
 #include "rit.h"
 
 void (*rit_cb)(void);
-
+//TODO the first parameter is not translated to any time base
 void init_rit(uint32_t us, void (*cb)(void)) {
   //set the callback
   rit_cb = cb;
   //power rit
   LPC_SC->PCONP |= (1 << 16);
+  //PCLK = CCLK
+  LPC_SC->PCLKSEL1 |= (0x01 << PCLK_RIT);
   //timer enable clear
   LPC_RIT->RICTRL |= (1 << RITENCLR);
   //set the compare value
--- lib/rit/rit.h
+++ lib/rit/rit.h
@@ -4,8 +4,14 @@
 #define RITINT 0
 #endif
 
+#ifndef PCLK_RIT
+#define PCLK_RIT 26
+#endif
+
 #ifndef RITENCLR
 #define RITENCLR 1
 #endif
 
 void init_rit(uint32_t us, void (*cb)(void));
+void set_rit(uint32_t us);
+void set_rit(uint32_t ms);
