df84fbb0b787c3161201e93737bc2b1897705192
Ben Noack <benjamin.noack@icloud.com>
2017-08-28 22:12:02 -0400

    comments

--- lib/uart/examples/echo.c
+++ lib/uart/examples/echo.c
@@ -1,15 +1,17 @@
 #include "blueboard.h"
-
+//send back what is recieved on interrupt
 void callback(char rx) {
   tx_uart(rx);
 }
 
 int main(int argc, char const *argv[]) {
+  //initialize the necessary peripherals
   init_leds();
   init_timers();
   init_uart(callback);
 
   while (1) {
+    //blinky lights
     led_on(LED1);
     timer_ms(100);
     led_off(LED1);
